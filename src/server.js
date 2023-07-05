ddd408ded54e6fb180ebd7596a6bfae57f95522f
BroskiBux <95157740+BroskiBux@users.noreply.github.com>
2023-07-05 14:15:04 -0400

    setup

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const express = require("express");
+const fs = require("fs");
+const { stringify } = require('querystring');
+const app = express();
+const bcrypt = require("bcrypt");
+const path = require("path");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("", (req, res) => {
+    const filepath = path.join(__dirname, "", "index.html");
+    res.sendFile(filepath);
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
